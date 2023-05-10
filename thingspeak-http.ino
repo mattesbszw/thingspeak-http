8da376fe964db4d2b3c66386bd0e0776294101be
mattesbszw <107214376+mattesbszw@users.noreply.github.com>
2023-05-10 08:18:27 +0200

    removing lcd display from code

--- thingspeak-http.ino
+++ thingspeak-http.ino
@@ -13,22 +13,14 @@
 #define ONE_WIRE_BUS 16
 #define DHTTYPE DHT11
 
-// uncomment to enable LCD-Display
-#define LCD_DISPLAY
-
 DHT dht(DHT_PIN, DHTTYPE);
-
-#ifdef LCD_DISPLAY
-  #include <LiquidCrystal_I2C.h>
-  LiquidCrystal_I2C lcd (0x27, 16,2);  
-#endif
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // WLAN-Zugangsdaten
 const char* ssid = "CPSLABOR";
-const char* password = "1234567890";
+const char* password = "A1234567890";
 
 WiFiClient client;
 
@@ -43,11 +35,6 @@ void setup(void){
   dht.begin(); // init DHT sensor
   sensors.begin(); // init DS18B20 sensors on OneWire bus
   
-#ifdef LCD_DISPLAY
-  lcd.init();               // init lcd1602 on I2C bus
-  lcd.backlight();          // backlight on
-#endif
-
   connectWiFi();
      
   // ThingSpeak API wird initialisiert
@@ -72,34 +59,19 @@ void connectWiFi() {
 
 void loop(void){ 
   
-  // lcd.clear();
-   
   sensors.requestTemperatures(); // Send the command to get temperatures
   String tmpDS = String(sensors.getTempCByIndex(0));
   Serial.print("Sensor 1(*C): ");
   Serial.println(tmpDS); 
-  
-#ifdef LCD_DISPLAY
-  lcd.setCursor(0,0);
-  lcd.print("DS: " + tmpDS);
-#endif
-  
+   
   float tmp = dht.readTemperature();
   String tmpDHT = "0";
   if (!isnan(tmp)) {
     tmpDHT = tmp;
   }
-
   Serial.print("Sensor 2(*C): "); 
   Serial.println(tmpDHT);
-  lcd.setCursor(0,1);
-  lcd.print("DHT: " + tmpDHT + "        ");
-
-#ifdef LCD_DISPLAY
-  lcd.setCursor(0,1);
-  lcd.print("DHT: " + tmpDHT + "        ");
-#endif
-
+ 
   ThingSpeak.setField(1, tmpDS);
   ThingSpeak.setField(2, tmpDHT);
   int ret = ThingSpeak.writeFields(tsChannelNumber, tsWriteAPIKey);
